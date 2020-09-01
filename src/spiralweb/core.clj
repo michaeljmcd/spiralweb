b4e290a003d66103580d46b6146acd4b69e41e99
Michael McDermott <michael.mcdermott@jbhunt.com>
2020-09-01 16:36:31 -0500

    Cleanliness is close to Godliness
    Re-ran cljfmt.

--- src/spiralweb/core.clj
+++ src/spiralweb/core.clj
@@ -266,70 +266,68 @@
 (t/merge-config! {:level :error})
 
 (defn is-code-chunk? [c]
- (= (:type c) :code))
+  (= (:type c) :code))
 
 (defn combine-code-chunks [result chunks]
- (let [chunk (first chunks)]
- (cond
-  (empty? chunks) 
-    result
-  (not (is-code-chunk? chunk)) 
-    (recur result (rest chunks))
-  (contains? result (:name chunk))
-   (recur
-    (update-in result [(:name chunk) :lines]
-     (fn [x] (concat (:lines chunk) x)))
-    (rest chunks))
-   :else
-   (recur (assoc result (:name chunk) chunk)
-    (rest chunks)))))
+  (let [chunk (first chunks)]
+    (cond
+      (empty? chunks)
+      result
+      (not (is-code-chunk? chunk))
+      (recur result (rest chunks))
+      (contains? result (:name chunk))
+      (recur
+       (update-in result [(:name chunk) :lines]
+                  (fn [x] (concat (:lines chunk) x)))
+       (rest chunks))
+      :else
+      (recur (assoc result (:name chunk) chunk)
+             (rest chunks)))))
 
 (defn output-option? [opt]
-(= "out" (-> opt :value :name)))
+  (= "out" (-> opt :value :name)))
 
 (defn output-path [c]
- (->
- (filter output-option? (:options c))
- first
- :value
- :value))
+  (->
+   (filter output-option? (:options c))
+   first
+   :value
+   :value))
 
-(defn has-output-path? 
- "Examines a chunk map and indicates whether there is an output path specified on the chunk."
- [c]
- (some? (output-path c)))
+(defn has-output-path?
+  "Examines a chunk map and indicates whether there is an output path specified on the chunk."
+  [c]
+  (some? (output-path c)))
 
 (defn is-chunk-reference? [c]
   (= :chunk-reference (:type c)))
 
-(defn build-chunk-crossrefs 
- "Accepts a sequence of chunks, some of which may be documentation chunks 
+(defn build-chunk-crossrefs
+  "Accepts a sequence of chunks, some of which may be documentation chunks 
   and some of which may be code chunks, and builds a map of maps indicating
   whether one chunk refers to another.
 
   {\"asdf\" : {\"def\": true, \"abc\": false }}
 
   This map is not sparse. It will include some entry for each chunk in the original list."
- [chunks]
- (letfn [(add-references [chunk result]
-          (let [ref-lines (filter is-chunk-reference? (:lines chunk))
-                chunk-adjacent (get result (:name chunk))
-                additions (interleave (map :name ref-lines) (repeat true))]
-          (if (empty? additions)
-           result
-           (assoc result (:name chunk)
-            (apply (partial assoc chunk-adjacent) additions)))
-          ))
+  [chunks]
+  (letfn [(add-references [chunk result]
+            (let [ref-lines (filter is-chunk-reference? (:lines chunk))
+                  chunk-adjacent (get result (:name chunk))
+                  additions (interleave (map :name ref-lines) (repeat true))]
+              (if (empty? additions)
+                result
+                (assoc result (:name chunk)
+                       (apply (partial assoc chunk-adjacent) additions)))))
 
-         (build-chunk-crossrefs-inner [chunks result]
-          (let [[c & cs] chunks]
-           (cond
-            (empty? chunks) result
-            :else (recur cs (add-references c result))
-           )
-          ))]
- (build-chunk-crossrefs-inner chunks (apply (partial assoc {})
-                                      (interleave (map :name (filter is-code-chunk? chunks)) (repeat {}))))))
+          (build-chunk-crossrefs-inner [chunks result]
+            (let [[c & cs] chunks]
+              (cond
+                (empty? chunks) result
+                :else (recur cs (add-references c result)))))]
+
+    (build-chunk-crossrefs-inner chunks (apply (partial assoc {})
+                                               (interleave (map :name (filter is-code-chunk? chunks)) (repeat {}))))))
 
 ; Let's take a step back and think about tangling. We want to take the input
 ; text and parse it. If there is an error, we stop and report the error.
@@ -339,105 +337,95 @@
 ; them out.
 
 (defn has-incoming-edges? [xrefs cn]
- (some #(some? (get % cn)) (vals xrefs)))
+  (some #(some? (get % cn)) (vals xrefs)))
 
 (defn find-candidate-nodes [xrefs]
- (filter (comp not (partial has-incoming-edges? xrefs)) (keys xrefs)))
+  (filter (comp not (partial has-incoming-edges? xrefs)) (keys xrefs)))
 
 (defn ts-inner [res xrefs]
- (let [candidates (find-candidate-nodes xrefs)]
-  (if (empty? candidates) [res xrefs]
-   (recur (cons (first candidates) res)
-          (dissoc xrefs (first candidates))))))
+  (let [candidates (find-candidate-nodes xrefs)]
+    (if (empty? candidates) [res xrefs]
+        (recur (cons (first candidates) res)
+               (dissoc xrefs (first candidates))))))
 
-(defn topologically-sort-chunks 
- "Accepts a map of code chunks (name -> value) and produces a topologically sorted list of chunk IDs."
- [chunks]
- (let [xrefs (build-chunk-crossrefs (vals chunks))
-       [sorted-names leftovers] (ts-inner [] xrefs)]
+(defn topologically-sort-chunks
+  "Accepts a map of code chunks (name -> value) and produces a topologically sorted list of chunk IDs."
+  [chunks]
+  (let [xrefs (build-chunk-crossrefs (vals chunks))
+        [sorted-names leftovers] (ts-inner [] xrefs)]
     (if (empty? leftovers)
-     sorted-names
-     "ERROR! Circular reference.")
- ))
+      sorted-names
+      "ERROR! Circular reference.")))
 
 (defn expand-refs [chunk all-chunks]
- (letfn [(expand-refs-inner [lines all-chunks result]
-          (let [line (first lines)]
-           (cond
-            (empty? lines) result
-            (is-chunk-reference? line) 
+  (letfn [(expand-refs-inner [lines all-chunks result]
+            (let [line (first lines)]
+              (cond
+                (empty? lines) result
+                (is-chunk-reference? line)
                 (recur (rest lines) all-chunks
-                 (concat (-> all-chunks (get (:name line)) :lines) result))
-            :else (recur (rest lines) all-chunks (cons line result))
-           )
-          )
-         )]
- (assoc chunk :lines
-  (expand-refs-inner (:lines chunk) all-chunks []))
-))
+                       (concat (-> all-chunks (get (:name line)) :lines) result))
+                :else (recur (rest lines) all-chunks (cons line result)))))]
+
+    (assoc chunk :lines
+           (expand-refs-inner (:lines chunk) all-chunks []))))
 
 (defn expand-chunks [queue chunks]
- (if (empty? queue)
-  chunks
-  (recur (rest queue)
-   (assoc chunks (first queue)
-    (expand-refs (get chunks (first queue)) chunks)))
-  )
-)
+  (if (empty? queue)
+    chunks
+    (recur (rest queue)
+           (assoc chunks (first queue)
+                  (expand-refs (get chunks (first queue)) chunks)))))
 
-(defn expand-code-refs 
- "Accepts a map of chunks (the key being the name of the chunk, value being the unique value."
- [chunks]
- (let [chunk-seq (topologically-sort-chunks chunks)]
-  (expand-chunks chunk-seq chunks)
- )
-)
+(defn expand-code-refs
+  "Accepts a map of chunks (the key being the name of the chunk, value being the unique value."
+  [chunks]
+  (let [chunk-seq (topologically-sort-chunks chunks)]
+    (expand-chunks chunk-seq chunks)))
 
 (defn refine-code-chunks [text]
- (let [parse-tree (web text)]
-  (if (or (failure? parse-tree) (not (empty? (second parse-tree))))
-   nil
-   (->> parse-tree
-        first
-        (filter is-code-chunk?)
-        (combine-code-chunks {})
-        expand-code-refs))))
+  (let [parse-tree (web text)]
+    (if (or (failure? parse-tree) (not (empty? (second parse-tree))))
+      nil
+      (->> parse-tree
+           first
+           (filter is-code-chunk?)
+           (combine-code-chunks {})
+           expand-code-refs))))
 
 (defn chunk-content [c]
- (->> c :lines (map :value) (apply str)))
+  (->> c :lines (map :value) (apply str)))
 
 (defn output-code-chunks [chunks]
- (doseq [chunk chunks]
-  (if (has-output-path? chunk)
-   (spit (output-path chunk) (chunk-content chunk))
-   (println (chunk-content chunk)))))
+  (doseq [chunk chunks]
+    (if (has-output-path? chunk)
+      (spit (output-path chunk) (chunk-content chunk))
+      (println (chunk-content chunk)))))
 
-(defn tangle 
- "Accepts a list of files, extracts code and writes it out."
- ([files output-chunks]
-    (doseq [f files]
+(defn tangle
+  "Accepts a list of files, extracts code and writes it out."
+  ([files output-chunks]
+   (doseq [f files]
      ; TODO: error handling
      (let [chunks (refine-code-chunks (slurp f))]
-      (output-code-chunks
-          (cond
-           (not (empty? output-chunks))
-               (filter (fn [x] 
-                        (contains? (set output-chunks) (:name x))) 
-                       (vals chunks))
-           (contains? chunks "*")
-            (list (get chunks "*"))
-           :else
-            (filter has-output-path? (vals chunks))
-          )))))
- ([files] (tangle files nil)))
-
+       (output-code-chunks
+        (cond
+          (not (empty? output-chunks))
+          (filter (fn [x]
+                    (contains? (set output-chunks) (:name x)))
+                  (vals chunks))
+          (contains? chunks "*")
+          (list (get chunks "*"))
+          :else
+          (filter has-output-path? (vals chunks)))))))
+  ([files] (tangle files nil)))
 
 (def cli-options
   [["-c" "--chunk CHUNK"]
    ["-f" "--help"]])
 
-(defn -main "The main entrypoint for running SpiralWeb as a command line tool." 
- [& args]
+(defn -main "The main entrypoint for running SpiralWeb as a command line tool."
+  [& args]
   (let [opts (parse-opts args cli-options)]
     (case (first (:arguments opts))
       "tangle" (tangle (rest (:arguments opts)))
